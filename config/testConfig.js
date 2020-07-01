683f5e65a4cafe66b716f35bf98ac0b50b75fc3f
Pritesh Gudge <priteshgudge@gmail.com>
2020-07-01 18:43:58 +0530

    Separating Data and Code test

--- config/testConfig.js
+++ config/testConfig.js
@@ -2,13 +2,14 @@
 var ExerciseC6A = artifacts.require("ExerciseC6A");
 var ExerciseC6B = artifacts.require("ExerciseC6B");
 var ExerciseC6C = artifacts.require("ExerciseC6C");
+var ExerciseC6CApp = artifacts.require("ExerciseC6CApp");
 
 var Config = async function(accounts) {
 
     // These test addresses are useful when you need to add
     // multiple users in test scripts
     let testAddresses = [
-        "0x69e1CB5cFcA8A311586e3406ed0301C06fb839a2",
+        "0x27D8D15CbC94527cAdf5eC14B69519aE23288B95",
         "0xF014343BDFFbED8660A9d8721deC985126f189F3",
         "0x0E79EDbD6A727CfeE09A2b1d0A59F7752d5bf7C9",
         "0x9bC1169Ca09555bf2721A5C9eC6D69c8073bfeB4",
@@ -24,6 +25,7 @@ var Config = async function(accounts) {
     let exerciseC6A = await ExerciseC6A.new();
     let exerciseC6B = await ExerciseC6B.new();
     let exerciseC6C = await ExerciseC6C.new();
+    let exerciseC6CApp = await ExerciseC6CApp.new(exerciseC6C.address);
 
     return {
         owner: owner,
@@ -31,6 +33,7 @@ var Config = async function(accounts) {
         exerciseC6A: exerciseC6A,
         exerciseC6B: exerciseC6B,
         exerciseC6C: exerciseC6C,
+        exerciseC6CApp: exerciseC6CApp
     }
 }
 
